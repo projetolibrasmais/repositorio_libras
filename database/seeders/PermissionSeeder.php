a58c365739be2cb33429aa842e3ec8e136321eee
Matheus Sousa <matheussousab2014@gmail.com>
2025-12-09 09:58:09 -0300

    feat: traduzir descrições de permissões para português

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -15,19 +15,19 @@ class PermissionSeeder extends Seeder
     {
         $permissions = [
             // Permissions
-            ['name' => 'view_permissions', 'description' => 'View permissions', 'guard_name' => 'web'],
+            ['name' => 'view_permissions', 'description' => 'Visualizar permissões', 'guard_name' => 'web'],
 
             //Roles
-            ['name' => 'view_roles', 'description' => 'View roles', 'guard_name' => 'web'],
-            ['name' => 'create_roles', 'description' => 'Create roles', 'guard_name' => 'web'],
-            ['name' => 'edit_roles', 'description' => 'Edit roles', 'guard_name' => 'web'],
-            ['name' => 'delete_roles', 'description' => 'Delete roles', 'guard_name' => 'web'],
+            ['name' => 'view_roles', 'description' => 'Visualizar funções', 'guard_name' => 'web'],
+            ['name' => 'create_roles', 'description' => 'Criar funções', 'guard_name' => 'web'],
+            ['name' => 'edit_roles', 'description' => 'Editar funções', 'guard_name' => 'web'],
+            ['name' => 'delete_roles', 'description' => 'Excluir funções', 'guard_name' => 'web'],
 
             //Categorias
-            ['name' => 'view_categorias', 'description' => 'View categorias', 'guard_name' => 'web'],
-            ['name' => 'create_categorias', 'description' => 'Create categorias', 'guard_name' => 'web'],
-            ['name' => 'edit_categorias', 'description' => 'Edit categorias', 'guard_name' => 'web'],
-            ['name' => 'delete_categorias', 'description' => 'Delete categorias', 'guard_name' => 'web'],
+            ['name' => 'view_categorias', 'description' => 'Visualizar categorias', 'guard_name' => 'web'],
+            ['name' => 'create_categorias', 'description' => 'Criar categorias', 'guard_name' => 'web'],
+            ['name' => 'edit_categorias', 'description' => 'Editar categorias', 'guard_name' => 'web'],
+            ['name' => 'delete_categorias', 'description' => 'Excluir categorias', 'guard_name' => 'web'],
         ];
 
         foreach ($permissions as $permission) {
